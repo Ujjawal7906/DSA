f664e0f40c9cc91f24bd37885d2fa1290b577479
Ujjawal7906 <ujjawalthakur896@gmail.com>
2026-05-22 22:01:08 +0530

    Add basic file

---
+++ basic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2; //declare a variable
+    cout<<"Enter num1: "; //Output
+    cin>>num1; //Input
+
+    cout<<"Enter num2: ";
+    cin>>num2;
+
+    cout<<"addition is "<< num1+num2 <<"\n"; //Addition of two numbers
+    cout<<"subtraction is "<< num1-num2<<"\n"; //Subtraction of two numbers
+    cout<<"multiplication is "<< num1*num2 <<"\n"; // Multiplication of two numbers
+    cout<<"division is "<< num1/num2 <<"\n"; // Division of two numbers
+    cout<<"modulus is "<< num1%num2 <<"\n"; // Modulus of two numbers
+}
