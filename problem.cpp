8cb8a18b4ffaf0df7a75e6c1728cb625e7f2e508
Ujjawal7906 <ujjawalthakur896@gmail.com>
2026-05-23 12:10:35 +0530

    Add problem file

---
+++ problem.cpp
@@ -0,0 +1,37 @@
+
+//Sum of all numbers from 1 to N which are divisible by 3 
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        if(i%3==0){
+            sum+=i;
+        }
+    }
+
+    cout<<sum;
+    return 0;
+}*/
+
+//print the factorial of number N
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n,fact=1;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for(int i=1;i<=n; i++){
+        fact*=i;
+    }
+
+    cout<<fact;
+    return 0;
+}*/
