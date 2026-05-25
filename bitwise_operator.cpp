3c247a7bbd28259d1a8c144f5fd9fda425d57ae4
Ujjawal7906 <ujjawalthakur896@gmail.com>
2026-05-25 22:21:17 +0530

    Add Bitwise Operator file

---
+++ bitwise_operator.cpp
@@ -0,0 +1,49 @@
+//Bitwise and
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=8;
+    cout<<(a&b);
+    return 0;
+}*/
+
+//Bitwise OR
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=8;
+    cout<<(a|b);
+    return 0;
+}*/
+
+//Bitwise XOR
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=8;
+    cout<<(a^b);
+    return 0;
+}*/
+
+//bitwise left shift
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=10,b=1;
+//     cout<<(a<<b);
+//     return 0;
+// }
+
+//Bitwise right shift
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10,b=1;
+    cout<<(a>>b);
+    return 0;
+}
