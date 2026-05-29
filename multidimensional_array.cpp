f49bade55b9b0d1192f54cbf91633093245f4647
Ujjawal7906 <ujjawalthakur896@gmail.com>
2026-05-29 18:50:39 +0530

    Updaten Multidimensional Array file

--- multidimensional_array.cpp
+++ multidimensional_array.cpp
@@ -1,16 +1,27 @@
-//declaration and initialisation of 2d array
+// declaration and initialisation of 2d array
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int matrix[2][3]={
+//         {1,2,3},
+//         {4,5,6}
+//     };
+//     for(int i=0;i<2;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<matrix[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// Size of multidimensional array
 
 #include<iostream>
 using namespace std;
 int main(){
-    int matrix[2][3]={
-        {1,2,3},
-        {4,5,6}
-    };
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    int arr[2][4];
+
+    cout<<sizeof(arr)<<" Bytes";
+    return 0;
 }
